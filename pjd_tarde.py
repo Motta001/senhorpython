619c2d0ae03d125875d8a2271647b8516f800094
Stefany Mota Santos <tetezinhamota58@gmail.com>
2025-10-22 20:19:26 +0000

    Python

---
+++ pjd_tarde.py
@@ -0,0 +1,45 @@
+# 1) IMPRIMA O SEU NOME 
+
+"""
+nome = "Stefany Mota"
+idade = 16
+print(nome, idade)
+
+# 2) IMPRIMA O NOME DA UNIDADE DO SENAI E SUA IDADE
+
+unidade = Senai 
+idade = 83
+print(nome, idade, unidade)
+
+
+a = 10
+b = 5
+print("A soma é: " a + b)
+
+# 3) IMPRIMA A SOMA DA SUA IDADE COM O ANO DO SEU NASCIMENTO 
+
+name = input(input("Digite a sua idade"))
+if idade > 18:
+    print("Voce e de maior")
+else:
+    print("Voce e de menor")
+
+
+# 5) CRIE UM CODIGO EM QUE SE A PESSOA NASCEU DEPOIS DOS ANOS 2000 É GERAÇÃO Z
+
+for i in range(5):
+    print("Contador: ", i)
+
+# 6) FAÇA UM CODIGO EM QUE IMPRIMA TODOS OS VALORES PARES DE 8 A 18
+
+def saudacao(nome):
+    print("Bem vindo,", nome)
+
+saudacao("Stefany")
+"""
+# 7) CRIE A FUNÇAO QUE RECEBE DOIS NUMEROS E RETORNA A SOMA DELES
+
+import random 
+
+cores = ["azul", "verde", "vermelho", "rosa"]
+cores_
